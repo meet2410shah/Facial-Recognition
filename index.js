bc4ef03a8d1ec69651272375b79efaf470b0f834
Meet Shah <meet2410shah@gmail.com>
2019-07-22 19:43:17 +0530

    Static Path Added.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index');
 })
+
+app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.listen(3000, () => {
   console.log('Server is listening to port 3000');
